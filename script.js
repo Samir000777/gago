14b061a7ffa7a0f5929f12de47f7a1249148345d
Samir000777 <111176325+Samir000777@users.noreply.github.com>
2022-11-28 00:12:46 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,18 @@
+// let name = "John"
+// let admin = name
+
+// console.log(
+//     admin + " это admin"
+// );
+
+
+let balance = 25000
+
+let car = 4480
+let food = 890
+
+let invoice = 5500
+let stock = 4200
+
+let totalBalance = balance-car-food+invoice+stock
+console.log("всего " + totalBalance);
